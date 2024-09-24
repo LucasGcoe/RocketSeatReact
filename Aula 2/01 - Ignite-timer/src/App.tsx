5e11b514650f7fb5c4af133d5f491f8c2143bd17
LucasGcoe <lucasoliveiragcoe@gmail.com>
2024-09-24 17:35:35 -0300

    adicionando e estilizando o button

--- Aula 2/01 - Ignite-timer/src/App.tsx
+++ Aula 2/01 - Ignite-timer/src/App.tsx
@@ -1,12 +1,18 @@
+import Button from "./components/Button";
+
 export default function App() {
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 
-  
+
 }
 
- 
+
